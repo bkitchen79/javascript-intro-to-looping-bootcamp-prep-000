791c48abbeb0a551a0ff2566046a79dda8dd9e36
Bradley Kitchen <bradkitchen2004@sbcglobal.net>
2017-12-18 22:42:34 +0000

    automatically backed up by learn

--- loops.js
+++ loops.js
@@ -1,7 +1,8 @@
 for (let i = 0; i < 25; i++) {
   if (i === 1) {
-    return "I am 1 strange loop";
+    array.push( "I am 1 strange loop.");
     }
-  else:
-    return "I am ${i} strange loops";
+  else {
+    array.push("I am ${i} strange loops.");
+    }
 }
